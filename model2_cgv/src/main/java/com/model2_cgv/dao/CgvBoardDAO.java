3b188f2373c272115e2f703486bd56341f0d0e99
DeEUS-22 <gowell_kim@naver.com>
2022-11-29 23:34:41 +0900

    22.11.29
    DAO
    - Repository & Mapper로 분리

--- model2_cgv/src/main/java/com/model2_cgv/dao/CgvBoardDAO.java
+++ model2_cgv/src/main/java/com/model2_cgv/dao/CgvBoardDAO.java
@@ -1,72 +1,43 @@
 package com.model2_cgv.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.model2_cgv.vo.CgvBoardVO;
 
-public class CgvBoardDAO extends DBConn{
+@Repository
+public class CgvBoardDAO {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
 	/**
 	 * insert : 게시글 등록
 	 */
 	public int insert(CgvBoardVO vo) {
-		int result = 0;
-		String sql = "insert into cgv_board "
-				+ " values('b_'||sequ_cgv_board.nextval, ?, ?, ?, ?, 0, sysdate)";
-		
-		try {
-			getPreparedStatement(sql);
-			pstmt.setString(1, vo.getBtitle());
-			pstmt.setString(2, vo.getBcontent());
-			pstmt.setString(3, vo.getBfile());
-			pstmt.setString(4, vo.getBsfile());
-			
-			result = pstmt.executeUpdate();
-			
-			close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return result;
+		return sqlSession.insert("mapper.board.insert", vo);
 	}
 	
 	/**
 	 * select : 게시글 전체 리스트
 	 */
 	public ArrayList<CgvBoardVO> select(int startCount, int endCount){
-		ArrayList<CgvBoardVO> list = new ArrayList<CgvBoardVO>();
-		String sql = "select rno, bid, btitle, bhits, bdate " +
-				 " from (select rownum rno, bid, btitle, bhits, to_char(bdate, 'yyyy-mm-dd') bdate " + 
-			" from (select bid, btitle, bhits, bdate from cgv_board " + 
-			"            order by bdate desc))" +
-			" where rno between  ? and ?"; 
 		
-		try {
-			getPreparedStatement(sql);
-			
-			pstmt.setInt(1, startCount); 
-			pstmt.setInt(2, endCount);
-			
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				CgvBoardVO vo = new CgvBoardVO();
-				vo.setRno(rs.getInt(1));
-				vo.setBid(rs.getString(2));
-				vo.setBtitle(rs.getString(3));
-				vo.setBhits(rs.getInt(4));
-				vo.setBdate(rs.getString(5));
-				
-				list.add(vo);
-			}
-			
-			close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		//파라미터를 Map으로 정의하기
+		Map<String, Integer> param = new HashMap<String, Integer>();
+		param.put("start", startCount);	
+		param.put("end", endCount);
 		
-		return list;
+		List<CgvBoardVO> list = sqlSession.selectList("mapper.board.list", param);
+		
+		
+		return (ArrayList<CgvBoardVO>)list;
 	}
 	
 	
@@ -74,122 +45,34 @@ public class CgvBoardDAO extends DBConn{
 	 * select : 게시글 상세 보기
 	 */
 	public CgvBoardVO select(String bid) {
-		CgvBoardVO vo = new CgvBoardVO();
-		String sql = "select bid, btitle, bcontent,bhits, bdate, bfile, bsfile "
-				+ " from cgv_board  where bid=?";
-		
-		try {
-			getPreparedStatement(sql);
-			pstmt.setString(1, bid);
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				vo.setBid(rs.getString(1));
-				vo.setBtitle(rs.getString(2));
-				vo.setBcontent(rs.getString(3));
-				vo.setBhits(rs.getInt(4));
-				vo.setBdate(rs.getString(5));
-				vo.setBfile(rs.getString(6));
-				vo.setBsfile(rs.getString(7));
-			}
-			
-			//close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return vo;
+		return sqlSession.selectOne("mapper.board.content",bid);
 	}
 	
 	/**
 	 * updateHits : 조회수 업데이트
 	 */
 	public int updateHits(String bid) {
-		int result = 0;
-		String sql = "update cgv_board set bhits=bhits+1 "
-				+ " where bid=?";
-		
-		try {
-			getPreparedStatement(sql);
-			pstmt.setString(1, bid);
-			result = pstmt.executeUpdate();
-			
-			close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		
-		return result;
+		return sqlSession.update("mapper.board.updatehits",bid);
 	}
 	
 	/**
 	 * update : 게시글 수정 
 	 */
-	public int update(CgvBoardVO vo) {
-		int result = 0;
-		String sql = "update cgv_board set btitle=?, bcontent=?, bfile=?, bsfile=? "
-				+ " where bid=?";
-		
-		try {
-			getPreparedStatement(sql);
-			pstmt.setString(1, vo.getBtitle());
-			pstmt.setString(2, vo.getBcontent());
-			pstmt.setString(3, vo.getBfile());
-			pstmt.setString(4, vo.getBsfile());
-			pstmt.setString(5, vo.getBid());
-			
-			result = pstmt.executeUpdate();
-			
-			close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return result;
+	public int update(CgvBoardVO vo) {		
+		return sqlSession.update("mapper.board.update",vo);
 	}
 	
 	/**
 	 * delete : 게시글 삭제
 	 */
 	public int delete(String bid) {
-		int result = 0;
-		String sql = "delete from cgv_board where bid=?";
-		
-		try {
-			getPreparedStatement(sql);  //pstmt 객체 생성
-			pstmt.setString(1, bid);
-			
-			result = pstmt.executeUpdate();
-			
-			close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return result;
+		return sqlSession.delete("mapper.board.delete",bid);
 	}
 	
 	/**
 	 * totalCount : 전체 로우수 출력
 	 */
 	public int totalCount() {
-		int result = 0;
-		String sql = "select count(*) from cgv_board";
-		
-		try {
-			getPreparedStatement(sql);
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				result = rs.getInt(1);
-			}
-			//close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return result;
+		return sqlSession.selectOne("mapper.board.totalcount");
 	}
 }
--- model2_cgv/src/main/java/com/model2_cgv/dao/CgvMemberDAO.java
+++ model2_cgv/src/main/java/com/model2_cgv/dao/CgvMemberDAO.java
@@ -1,163 +1,64 @@
 package com.model2_cgv.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import com.model2_cgv.vo.CgvBoardVO;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.model2_cgv.vo.CgvMemberVO;
+import com.model2_cgv.vo.SessionVO;
 
-public class CgvMemberDAO extends DBConn{
+@Repository
+public class CgvMemberDAO {
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
 	/**
 	 * insert : 회원가입 
 	 */
 	public int insert(CgvMemberVO vo) {
-		int result = 0;
-		String sql = "insert into cgv_member "
-				+ " values(?,?,?,?,?,?,?,?,?,?,?,?,sysdate)";
-		
-		try {
-			getPreparedStatement(sql);
-			pstmt.setString(1, vo.getId());
-			pstmt.setString(2, vo.getPass());
-			pstmt.setString(3, vo.getName());
-			pstmt.setString(4, vo.getGender());
-			pstmt.setString(5, vo.getEmail());
-			pstmt.setString(6, vo.getZonecode());
-			pstmt.setString(7, vo.getAddr1());
-			pstmt.setString(8, vo.getAddr2());
-			pstmt.setString(9, vo.getHp());
-			pstmt.setString(10, vo.getPnumber());
-			pstmt.setString(11, vo.getHobbylist());
-			pstmt.setString(12, vo.getIntro());
-			
-			result = pstmt.executeUpdate();
-			
-			close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return result;
+		//sqlSession 객체의 메소드를 호출하여 실행결과를 가져옴
+		return sqlSession.insert("mapper.member.join", vo);
 	}//insert-end
 	
 	/**
 	 * select : 로그인
 	 */
-	public int select(CgvMemberVO vo) {
-		int result = 0;
-		String sql = "select count(*) from cgv_member "
-				+ " where id=? and pass=?";
-		
-		try {
-			getPreparedStatement(sql);
-			pstmt.setString(1, vo.getId());
-			pstmt.setString(2, vo.getPass());
-			
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				result = rs.getInt(1);
-			}
-			
-			close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return result;
-	}
+	public SessionVO select(CgvMemberVO vo) {
+		return sqlSession.selectOne("mapper.member.login",vo);
+	}	
 	
 	/**
 	 * selectAll : 회원 전체 리스트
 	 */
-	public ArrayList<CgvMemberVO> selectAll(int startCount, int endCount){
-		ArrayList<CgvMemberVO> list = new ArrayList<CgvMemberVO>();
-		String sql = " select rno, id, name, pnumber, mdate "
-				+ " from (select rownum rno, id, name, pnumber, to_char(mdate,'yyyy-mm-dd') mdate " + 
-				" from (select id, name, pnumber, mdate from cgv_member " + 
-				"            order by mdate desc)) "
-				+ " where rno between ? and ?"; 
+	public ArrayList<CgvMemberVO> selectAll(int startCount, int endCount){		
+		//Map 타입의 객체를 생성하고 start, end라는 키이름으로 파라미터를 넘긴다.
+		Map<String,Integer> param = new HashMap<String,Integer>();
+		param.put("start", startCount);
+		param.put("end", endCount);
 		
-		try {
-			getPreparedStatement(sql);
-			pstmt.setInt(1, startCount);
-			pstmt.setInt(2, endCount);
-			
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				CgvMemberVO vo = new CgvMemberVO();
-				vo.setRno(rs.getInt(1));
-				vo.setId(rs.getString(2));
-				vo.setName(rs.getString(3));
-				vo.setPnumber(rs.getString(4));
-				vo.setMdate(rs.getString(5));
-				
-				list.add(vo);
-			}
-			
-			close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		List<CgvMemberVO> list = sqlSession.selectList("mapper.member.listAll", param);
 		
-		return list;
+		return (ArrayList<CgvMemberVO>)list;
 	}
 	
 	/**
 	 * selectContent : 회원 상세 정보
 	 */
 	public CgvMemberVO selectContent(String id) {
-		CgvMemberVO vo = new CgvMemberVO();
-		String sql = "select id, name, zoncode, addr1, addr2, pnumber "
-				+ " , hobbylist, mdate, intro  "
-				+ " from cgv_member where id=?";
-		
-		try {
-			getPreparedStatement(sql);
-			pstmt.setString(1, id);
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				vo.setId(rs.getString(1));
-				vo.setName(rs.getString(2));
-				vo.setZonecode(rs.getString(3));
-				vo.setAddr1(rs.getString(4));
-				vo.setAddr2(rs.getString(5));
-				vo.setPnumber(rs.getString(6));
-				vo.setHobbylist(rs.getString(7));
-				vo.setMdate(rs.getString(8));
-				vo.setIntro(rs.getString(9));
-			}
-			
-			close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return vo;
+		return sqlSession.selectOne("mapper.member.content", id);	
 	}
 	
 	/**
 	 * totalCount : 전체 로우수 출력
 	 */
-	public int totalCount() {
-		int result = 0;
-		String sql = "select count(*) from cgv_member";
-		
-		try {
-			getPreparedStatement(sql);
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				result = rs.getInt(1);
-			}
-			//close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return result;
+	public int totalCount() {		
+		return sqlSession.selectOne("mapper.member.total_count");
 	}
 	
 }//CgvMemberDAO-end
